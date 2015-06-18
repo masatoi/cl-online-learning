7865ca5677c147b47ccf114f724011a0989e2b5a
Satoshi Imai <satoshi.imai@gmail.com>
2015-06-18 14:48:01 +0900

    Added SCW-I & SCW-II learner and changed to CLOS style.

--- src/cl-online-learning.lisp
+++ src/cl-online-learning.lisp
@@ -7,6 +7,28 @@
 
 (in-package :cl-online-learning)
 
+;; defclass-simplified: Simplified definition of classes which similar to definition of structure.
+(defmacro defclass$ (class-name superclass-list &body body)
+  "Simplified definition of classes which similar to definition of structure.
+ [Example]
+  (defclass$ agent (superclass1 superclass2)
+    currency
+    position-list
+    (position-upper-bound 1)
+    log
+    money-management-rule)
+=> #<STANDARD-CLASS AGENT>"
+  `(defclass ,class-name (,@superclass-list)
+     ,(mapcar (lambda (slot)
+		(let* ((slot-symbol (if (listp slot) (car slot) slot))
+		       (slot-name (symbol-name slot-symbol))
+		       (slot-initval (if (listp slot) (cadr slot) nil)))
+		  (list slot-symbol
+			:accessor (intern (concatenate 'string slot-name "-OF"))
+			:initarg (intern slot-name :KEYWORD)
+			:initform slot-initval)))
+	      body)))
+
 ;;; Signum function
 (defun sign (x)
   (if (> x 0d0) 1d0 -1d0))
@@ -16,23 +38,28 @@
   (+ (inner-product weight input) bias))
 
 ;;; Definition CLOS object
-(defclass learner ()
-  ((input-dimension :accessor input-dimension-of :initarg :input-dimension)
-   (weight :accessor weight-of :initarg :weight)
-   (bias :accessor bias-of :initarg :bias)))
+(defclass$ learner () input-dimension weight bias)
+
+;;; Definition generic functions
+(defgeneric predict (learner input))
+(defgeneric test (learner test-data  &key quiet-p))
+(defgeneric update (learner input training-label))
+(defgeneric train (learner training-data))
+(defgeneric train-with-interim-test (learner training-data test-data span))
 
 ;;; Prediction
 (defmethod predict ((learner learner) input)
   (sign (f input (weight-of learner) (bias-of learner))))
 
 ;;; Testing with a test data list (1-pass)
-(defmethod test ((learner learner) test-data)
+(defmethod test ((learner learner) test-data &key (quiet-p nil))
   (let* ((len (length test-data))
 	 (n-correct (count-if (lambda (datum)
 				(= (predict learner (cdr datum)) (car datum)))
 			      test-data))
 	 (accuracy (* (/ n-correct len) 100.0)))
-    (format t "Accuracy: ~f%, Correct: ~A, Total: ~A~%" accuracy n-correct len)
+    (if (not quiet-p)
+      (format t "Accuracy: ~f%, Correct: ~A, Total: ~A~%" accuracy n-correct len))
     (values accuracy n-correct len)))
 
 ;;; Update inner parameters destructively (1step)
@@ -44,8 +71,18 @@
     (update learner (cdr datum) (car datum)))
   learner)
 
+(defmethod train-with-interim-test ((learner learner) training-data test-data span)
+  (let ((result nil))
+    (loop for i from 0 to (1- (length training-data))
+	  for datum in training-data
+	  do
+       (update learner (cdr datum) (car datum))
+       (when (zerop (mod i span))
+	 (push (test learner test-data :quiet-p t) result)))
+    (nreverse result)))
+
 ;;; Perceptron
-(defclass perceptron (learner) ())
+(defclass$ perceptron (learner))
 
 (defun make-perceptron (input-dimension)
   (check-type input-dimension integer)
@@ -66,12 +103,8 @@
   learner)
 
 ;;; Averaged Perceptron
-(defclass averaged-perceptron (perceptron)
-  ((data-size :accessor data-size-of :initarg :data-size)
-   (averaged-weight :accessor averaged-weight-of :initarg :averaged-weight)
-   (averaged-bias :accessor averaged-bias-of :initarg :averaged-bias)
-   (counter :accessor counter-of :initarg :counter)
-   (tmp-vec :accessor tmp-vec-of :initarg :tmp-vec)))
+(defclass$ averaged-perceptron (perceptron)
+  data-size averaged-weight averaged-bias counter tmp-vec)
 
 (defun make-averaged-perceptron (input-dimension data-size)
   (check-type input-dimension integer)
@@ -109,10 +142,7 @@
   (sign (f input (averaged-weight-of learner) (averaged-bias-of learner))))
 
 ;;; Linear SVM
-(defclass svm (learner)
-  ((learning-rate :accessor learning-rate-of :initarg :learning-rate)
-   (regularization-parameter :accessor regularization-parameter-of :initarg :regularization-parameter)
-   (tmp-vec :accessor tmp-vec-of :initarg :tmp-vec)))
+(defclass$ svm (learner) learning-rate regularization-parameter tmp-vec)
 
 (defun make-svm (input-dimension learning-rate regularization-parameter)
   (check-type input-dimension integer)
@@ -143,31 +173,26 @@
   learner)
 
 ;;; AROW
-(defclass arow (learner)
-  ((gamma :accessor gamma-of :initarg :gamma)
-   (weight-confidence :accessor weight-confidence-of :initarg :weight-confidence)
-   (bias-confidence :accessor bias-confidence-of :initarg :bias-confidence)
-   (tmp-vec1 :accessor tmp-vec1-of :initarg :tmp-vec1)
-   (tmp-vec2 :accessor tmp-vec2-of :initarg :tmp-vec2)))
+(defclass$ arow (learner) gamma sigma sigma0 tmp-vec1 tmp-vec2)
 
 (defun make-arow (input-dimension gamma)
   (check-type input-dimension integer)
   (check-type gamma double-float)
   (make-instance 'arow
      :input-dimension input-dimension
-     :weight (make-dvec input-dimension 0d0)
-     :bias 0d0
+     :weight (make-dvec input-dimension 0d0) ; mu
+     :bias 0d0                               ; mu0
      :gamma gamma
-     :weight-confidence (make-dvec input-dimension 1d0)
-     :bias-confidence 1d0
+     :sigma (make-dvec input-dimension 1d0)
+     :sigma0 1d0
      :tmp-vec1 (make-dvec input-dimension 0d0)
      :tmp-vec2 (make-dvec input-dimension 0d0)))
 
 (defmethod update ((learner arow) input training-label)
   (let* ((loss (- 1d0 (* training-label (f input (weight-of learner) (bias-of learner))))))
     (if (> loss 0d0)
-      (let* ((beta (/ 1d0 (+ (bias-confidence-of learner)
-			     (inner-product (diagonal-matrix-multiplication (weight-confidence-of learner)
+      (let* ((beta (/ 1d0 (+ (sigma0-of learner)
+			     (inner-product (diagonal-matrix-multiplication (sigma-of learner)
 									    input
 									    (tmp-vec1-of learner))
 					    input)
@@ -177,14 +202,157 @@
 	(v-scale (tmp-vec1-of learner) (* alpha training-label) (tmp-vec2-of learner))
 	(v+ (weight-of learner) (tmp-vec2-of learner) (weight-of learner))
 	;; Update bias
-	(setf (bias-of learner) (+ (bias-of learner) (* alpha (bias-confidence-of learner) training-label)))
-	;; Update weight-confidence
+	(setf (bias-of learner) (+ (bias-of learner) (* alpha (sigma0-of learner) training-label)))
+	;; Update sigma
 	(diagonal-matrix-multiplication (tmp-vec1-of learner) (tmp-vec1-of learner) (tmp-vec1-of learner))
 	(v-scale (tmp-vec1-of learner) beta (tmp-vec1-of learner))
-	(v- (weight-confidence-of learner) (tmp-vec1-of learner) (weight-confidence-of learner))
-	;; Update bias-confidence
-	(setf (bias-confidence-of learner)
-	      (- (bias-confidence-of learner)
-		 (* beta (bias-confidence-of learner)
-		    (bias-confidence-of learner)))))))
+	(v- (sigma-of learner) (tmp-vec1-of learner) (sigma-of learner))
+	;; Update sigma0
+	(setf (sigma0-of learner)
+	      (- (sigma0-of learner)
+		 (* beta (sigma0-of learner)
+		    (sigma0-of learner)))))))
+  learner)
+
+;;; SCW-I
+
+;; Approximation of error function
+(defun inverse-erf (x)
+  (let* ((a (/ (* 8d0 (- pi 3d0))
+	       (* 3d0 pi (- 4d0 pi))))
+	 (c2/pia (/ 2d0 pi a))
+	 (ln1-x^2 (log (- 1d0 (* x x))))
+	 (comp (+ c2/pia (/ ln1-x^2 2d0))))
+  (* (sign x)
+     (sqrt (- (sqrt (- (* comp comp) (/ ln1-x^2 a)))
+	      comp)))))
+
+(defun probit (p)
+  (* (sqrt 2d0)
+     (inverse-erf (- (* 2d0 p) 1d0))))
+
+(defclass$ scw1 (learner)
+  ;; Meta parameters
+  eta C
+  ;; Internal parameters
+  phi psi zeta sigma sigma0
+  tmp-vec1 tmp-vec2)
+
+(defun make-scw1 (input-dimension eta C)
+  (check-type input-dimension integer)
+  (check-type eta double-float)
+  (check-type C double-float)
+  (assert (< 0d0 eta 1d0))
+  (let* ((phi (probit eta))
+	 (psi (+ 1d0 (/ (* phi phi) 2d0)))
+	 (zeta (+ 1d0 (* phi phi))))
+    (make-instance 'scw1
+       :input-dimension input-dimension
+       :weight (make-dvec input-dimension 0d0)
+       :bias 0d0
+       :eta eta
+       :C C
+       :phi phi
+       :psi psi
+       :zeta zeta
+       :sigma (make-dvec input-dimension 1d0)
+       :sigma0 1d0
+       :tmp-vec1 (make-dvec input-dimension 0d0)
+       :tmp-vec2 (make-dvec input-dimension 0d0))))
+
+(defmethod update ((learner scw1) input training-label)
+  (let* ((phi (phi-of learner))
+	 (m (* training-label (f input (weight-of learner) (bias-of learner))))
+	 (v (+ (sigma0-of learner)
+	       (inner-product (diagonal-matrix-multiplication (sigma-of learner) input (tmp-vec1-of learner))
+			      input)))
+	 (loss (- (* phi (sqrt v)) m)))
+    (if (> loss 0d0)
+      (let* ((psi (psi-of learner))
+	     (zeta (zeta-of learner))
+	     (alpha (min (C-of learner)
+			 (max 0d0
+			      (- (sqrt (+ (/ (* m m phi phi phi phi) 4d0)
+					  (* v phi phi zeta)))
+				 (* m psi)))))
+	     (u (let ((base (- (sqrt (+ (* alpha alpha v v phi phi) (* 4d0 v))) (* alpha v phi))))
+		  (/ (* base base) 4d0)))
+	     (beta (/ (* alpha phi)
+		      (+ (sqrt u) (* v alpha phi)))))
+	;; Update weight
+	(v-scale (tmp-vec1-of learner) (* alpha training-label) (tmp-vec2-of learner))
+	(v+ (weight-of learner) (tmp-vec2-of learner) (weight-of learner))
+	;; Update bias
+	(setf (bias-of learner) (+ (bias-of learner) (* alpha (sigma0-of learner) training-label)))
+	;; Update sigma
+	(diagonal-matrix-multiplication (tmp-vec1-of learner) (tmp-vec1-of learner) (tmp-vec1-of learner))
+	(v-scale (tmp-vec1-of learner) beta (tmp-vec1-of learner))
+	(v- (sigma-of learner) (tmp-vec1-of learner) (sigma-of learner))
+	;; Update sigma0
+	(setf (sigma0-of learner)
+	      (- (sigma0-of learner)
+		 (* beta (sigma0-of learner)
+		    (sigma0-of learner)))))))
+  learner)
+
+;;; SCW-II
+
+(defclass$ scw2 (learner)
+  ;; Meta parameters
+  eta C
+  ;; Internal parameters
+  phi sigma sigma0
+  tmp-vec1 tmp-vec2)
+
+(defun make-scw2 (input-dimension eta C)
+  (check-type input-dimension integer)
+  (check-type eta double-float)
+  (check-type C double-float)
+  (assert (< 0d0 eta 1d0))
+  (let ((phi (probit eta)))
+    (make-instance 'scw2
+       :input-dimension input-dimension
+       :weight (make-dvec input-dimension 0d0)
+       :bias 0d0
+       :eta eta
+       :C C
+       :phi phi
+       :sigma (make-dvec input-dimension 1d0)
+       :sigma0 1d0
+       :tmp-vec1 (make-dvec input-dimension 0d0)
+       :tmp-vec2 (make-dvec input-dimension 0d0))))
+
+(defmethod update ((learner scw2) input training-label)
+  (let* ((phi (phi-of learner))
+	 (m (* training-label (f input (weight-of learner) (bias-of learner))))
+	 (v (+ (sigma0-of learner)
+	       (inner-product (diagonal-matrix-multiplication (sigma-of learner) input (tmp-vec1-of learner))
+			      input)))
+	 (loss (- (* phi (sqrt v)) m)))
+    (if (> loss 0d0)
+      (let* ((n (+ v (/ 1d0 (* 2d0 (C-of learner)))))
+	     (gamma (* phi
+		       (sqrt (+ (* phi phi m m v v)
+				(* 4d0 n v (+ n (* v phi phi)))))))
+	     (alpha (max 0d0
+			 (/ (- gamma (+ (* 2d0 m n) (* phi phi m v)))
+			    (* 2d0 (+ (* n n) (* n v phi phi))))))
+	     (u (let ((base (- (sqrt (+ (* alpha alpha v v phi phi) (* 4d0 v))) (* alpha v phi))))
+		  (/ (* base base) 4d0)))
+	     (beta (/ (* alpha phi)
+		      (+ (sqrt u) (* v alpha phi)))))
+	;; Update weight
+	(v-scale (tmp-vec1-of learner) (* alpha training-label) (tmp-vec2-of learner))
+	(v+ (weight-of learner) (tmp-vec2-of learner) (weight-of learner))
+	;; Update bias
+	(setf (bias-of learner) (+ (bias-of learner) (* alpha (sigma0-of learner) training-label)))
+	;; Update sigma
+	(diagonal-matrix-multiplication (tmp-vec1-of learner) (tmp-vec1-of learner) (tmp-vec1-of learner))
+	(v-scale (tmp-vec1-of learner) beta (tmp-vec1-of learner))
+	(v- (sigma-of learner) (tmp-vec1-of learner) (sigma-of learner))
+	;; Update sigma0
+	(setf (sigma0-of learner)
+	      (- (sigma0-of learner)
+		 (* beta (sigma0-of learner)
+		    (sigma0-of learner)))))))
   learner)
