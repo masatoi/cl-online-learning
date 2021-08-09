8c644f9d2b475104a9092d1f9dff9befe0bbfcfd
Satoshi Imai <satoshi.imai@gmail.com>
2021-08-09 09:47:39 +0900

    Fix mushrooms test

--- example/classification/mushrooms.lisp
+++ example/classification/mushrooms.lisp
@@ -17,28 +17,9 @@
 (defparameter mushrooms-train (clol.utils:read-data "/home/wiz/datasets/mushrooms-train" mushrooms-dim))
 (defparameter mushrooms-test (clol.utils:read-data "/home/wiz/datasets/mushrooms-test" mushrooms-dim))
 
-(defparameter arow-learner (make-arow mushrooms-dim 10d0))
+(defparameter arow-learner (make-arow mushrooms-dim 10.0))
 (loop repeat 10 do
   (train arow-learner mushrooms-train)
   (test arow-learner mushrooms-test))
 
 ;; Accuracy: 93.36158%, Correct: 1983, Total: 2124
-
-(defparameter mushrooms-mul-train
-  (let ((target (mapcar (lambda (datum)
-                          (if (> (car datum) 0d0) 1 0))
-                        mushrooms-train))
-        (input (mapcar #'cdr mushrooms-train)))
-    (mapcar #'cons target input)))
-
-(defparameter mushrooms-mul-test
-  (let ((target (mapcar (lambda (datum)
-                          (if (> (car datum) 0d0) 1 0))
-                        mushrooms-test))
-        (input (mapcar #'cdr mushrooms-test)))
-    (mapcar #'cons target input)))
-
-(defparameter arow-mul-learner (make-one-vs-rest mushrooms-dim 2 'arow 10d0))
-(loop repeat 10 do
-  (train arow-mul-learner mushrooms-mul-train)
-  (test arow-mul-learner mushrooms-mul-test))
